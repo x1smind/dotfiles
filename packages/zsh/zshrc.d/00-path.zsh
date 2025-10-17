fc99e0b8bb93f45eaca3b62d355d540c976c478e
x1smind <business@x1smind.com>
2025-10-17 10:39:20 +0000

    refactor(bootstrap): drop linuxbrew on linux
    Fixes #8

--- packages/zsh/zshrc.d/00-path.zsh
+++ packages/zsh/zshrc.d/00-path.zsh
@@ -1,6 +1,5 @@
-# Homebrew (macOS or Linux)
+# Homebrew (macOS)
 if [ -x /opt/homebrew/bin/brew ]; then eval "$(/opt/homebrew/bin/brew shellenv)"; fi
-if [ -x /home/linuxbrew/.linuxbrew/bin/brew ]; then eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"; fi
 
 # Local bins
 export PATH="$HOME/bin:$HOME/.local/bin:$PATH"
